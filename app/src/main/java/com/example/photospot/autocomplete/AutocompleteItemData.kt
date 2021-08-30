080a354d2a7e0131ba2a17d1241720320566bc84
Milan Dol <milandol321@gmail.com>
2021-08-30 14:42:36 +0200

    Add newlines at end of file

--- app/src/main/java/com/example/photospot/autocomplete/AutocompleteItemData.kt
+++ app/src/main/java/com/example/photospot/autocomplete/AutocompleteItemData.kt
@@ -1,5 +1,7 @@
 package com.example.photospot.autocomplete
 
-class AutocompleteItemData(var primaryText: String, var secondaryText: String, var placeId: String) {
-
-}
+class AutocompleteItemData(
+    var primaryText: String,
+    var secondaryText: String,
+    var placeId: String
+)
