37edbaeb037c205c42e9edb16ac70fb8bfce3a05
JustMilan <milandol321@gmail.com>
2021-09-07 01:46:12 +0200

    Add line endings at end of file and remove unnecesary braces

--- app/src/main/java/com/example/photospot/authentication/GoogleSignInClientWrapper.kt
+++ app/src/main/java/com/example/photospot/authentication/GoogleSignInClientWrapper.kt
@@ -2,4 +2,4 @@ package com.example.photospot.authentication
 
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 
-data class GoogleSignInClientWrapper(val googleSignInClient: GoogleSignInClient) {}
+data class GoogleSignInClientWrapper(val googleSignInClient: GoogleSignInClient)
